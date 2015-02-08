baa6b0a36b425af7edbec40535d01eb9aef196d6
teramonagi <teramonagi@gmail.com>
2015-02-08 18:37:47 +0900

    Updated to choose any interest rate term

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+#Library
 library(shiny)
 library(shinythemes)
 library(shinydashboard)
@@ -5,18 +6,22 @@ library(pipeR)
 library(zoo)
 library(dplyr)
 library(dygraphs)
+#Constant
+HEADER <- paste0(c(1:10,15,20,25,30,40), "Y")
+URL_CURRENT <- "http://www.mof.go.jp/english/jgbs/reference/interest_rate/jgbcme.csv"
+URL_OLD     <- "http://www.mof.go.jp/english/jgbs/reference/interest_rate/historical/jgbcme_all.csv"
 ################################################################
 #UI component
 #Header
 dashboard_header <- dashboardHeader(
-  title = "JGB Viewer(Beta)",
+  title="JGB Viewer(Beta)",
   dropdownMenuOutput("messageMenu")
 )
 #Sidebar
 dashboard_sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Main",  tabName = "main", icon = icon("dashboard")),
-    menuItem("About", tabName = "about", icon = icon("info-circle"))
+    menuItem("Main",  tabName="main",  icon=icon("dashboard")),
+    menuItem("About", tabName="about", icon=icon("info-circle"))
   )
 )
 #Body
@@ -26,14 +31,23 @@ dashboard_body <- dashboardBody(
     tabItem(
       tabName = "main",
       fluidRow(
-        dygraphOutput("dygraph", width = "80%")
+        box(
+          title="One term year plot", status="primary", solidHeader=TRUE, collapsible=TRUE, width=12,
+          dygraphOutput("dygraph")
+        )
+      ),
+      fluidRow(
+        box(
+          title="Choose the term of Interest Rate", status="primary", solidHeader=TRUE, width=8,
+          selectInput("term", "", HEADER, selected="10Y")
+        )
       )
     ),    
     # Second tab content
     tabItem(
       tabName = "about",
       h2("What's this?"),
-      p("This is a  Japanese Government Bond(JGB) rate viewer."),
+      p("This is a Japanese Government Bond(JGB) rate viewer application developed by Shiny and R."),
       h2("Code"),
       p("You can download all codes from the following URL on Github."),
       a(href="https://github.com/teramonagi/JGBViewer", "https://github.com/teramonagi/JGBViewer")
@@ -49,16 +63,14 @@ ui <- dashboardPage(
 ################################################################
 #Server
 server <- function(input, output) {
-  HEADER <- paste0(c(1:10,15,20,25,30,40), "Y")
-  URL_CURRENT <- "http://www.mof.go.jp/english/jgbs/reference/interest_rate/jgbcme.csv"
-  URL_OLD     <- "http://www.mof.go.jp/english/jgbs/reference/interest_rate/historical/jgbcme_all.csv"
   jgb_current <- read.csv(URL_CURRENT, stringsAsFactors=FALSE, na.strings = "-") %>>% setNames(c("Date", HEADER))
   jgb_old <- read.csv(URL_OLD, stringsAsFactors=FALSE, na.strings = "-") %>>% setNames(c("Date", HEADER))
   jgb <- rbind(jgb_old, jgb_current) %>>% read.zoo
   output$dygraph  <- renderDygraph({
-    jgb[,"10Y"] %>>%
-      dygraph(main="JGB Interest Rate(10Y only") %>>% 
-      dyRangeSelector(dateWindow = c("2012-01-01", as.character(Sys.Date())))
+    selected_term <- input$term
+    jgb[, selected_term] %>>%
+      dygraph(main=paste0("JGB Interest Rate - ", selected_term, " -")) %>>% 
+      dyRangeSelector(dateWindow = c(as.character(Sys.Date()-365), as.character(Sys.Date())))
   })
   output$messageMenu <- renderDropdownMenu({
     dropdownMenu(
